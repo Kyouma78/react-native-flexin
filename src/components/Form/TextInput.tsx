465c86e2b74813d9dbb2082972e4f53fbfcb15d4
Ryadh <rkherroub@forepaas.com>
2021-01-09 17:02:04 +0100

    Add Components

---
+++ src/components/Form/TextInput.tsx
@@ -0,0 +1,54 @@
+import React, { forwardRef } from 'react'
+import { StyleSheet, TextInput as RNTextInput, TextInputProps as RNTextInputProps } from 'react-native'
+import {Feather as Icon} from "@expo/vector-icons"
+
+import { Box, RoundedIcon, useTheme } from '..'
+
+interface TextInputProps extends RNTextInputProps {
+  icon: string
+  touched?: boolean
+  error?: string
+}
+
+const TextInput = forwardRef<RNTextInput, TextInputProps>(
+  ({ icon, touched, error, ...props }, ref) => {
+
+  const theme = useTheme()
+  const SIZE = theme.borderRadii.m * 2
+  const reColor = !touched ? "text" : error ? "danger" : "primary"
+  const color = theme.colors[reColor]
+
+  return (
+    <Box
+      flexDirection="row"
+      height={48}
+      alignItems="center"
+      borderRadius="s"
+      borderWidth={StyleSheet.hairlineWidth}
+      borderColor={reColor}
+      padding="s"
+    >
+      <Box padding="s">
+        <Icon name={icon} size={16} {...{ color }} />
+      </Box>
+      <Box flex={1}>
+        <RNTextInput
+          underlineColorAndroid="transparent"
+          placeholderTextColor={color}
+          {...{ref}}
+          {...props}
+          />
+      </Box>
+      {touched && (
+        <RoundedIcon
+          name={!error ? "check" : "x"}
+          size={SIZE}
+          backgroundColor={!error ? "primary" : "danger"}
+          color="background"
+        />
+      )}
+        </Box>    
+      )}
+  )
+
+export default TextInput
